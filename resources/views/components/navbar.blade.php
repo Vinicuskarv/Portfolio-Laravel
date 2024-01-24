41241111fcb7f8f8cc0b216b074fb94554664d30
Vinicius <karvvinicius@gmail.com>
2024-01-24 22:06:57 +0000

    add tailwind

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,8 @@
+<nav class="navbar bg-white dark:bg-black">
+  <div class="container-fluid">
+    <div class="content">
+        <div class="text">VINICIUS</div>
+        <div class="text">VINICIUS</div>
+    </div>
+  </div>
+</nav>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php"
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+
