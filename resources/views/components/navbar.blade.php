19cd834fc2c648249ddff1a7e795e9284114acb9
Vinicius <karvvinicius@gmail.com>
2024-01-27 19:24:44 +0000

    add-v-navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,8 +1,16 @@
-<nav class="navbar bg-white dark:bg-black">
-  <div class="container-fluid">
-    <div class="content">
-        <div class="text">VINICIUS</div>
-        <div class="text">VINICIUS</div>
+<nav class="navbar">
+  <div class="text">
+    <i class="fa-brands fa-laravel"></i>
+    <h3>VINICIUS</h3>
+  </div>
+  <div class="contant">
+    <div>
+      
+      <i class="icon fa-solid fa-earth-americas"></i>
     </div>
+    <label class="switch">
+      <input type="checkbox">
+      <span class="slider"></span>
+    </label>
   </div>
 </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,18 +8,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{ asset('css/components/navbar.css') }}" rel="stylesheet">
     <link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
-
+    <script src="https://kit.fontawesome.com/db74d9e1d4.js" crossorigin="anonymous"></script>
 
     <title>Document</title>
     
 </head>
-<body class="bg-white dark:bg-black">
+<body>
 
     @include('./components/navbar')
 
     
     <div class="container bg-white dark:bg-black">
-        <h1>olá</h1>
+        <h1>Olá</h1>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js"></script>
