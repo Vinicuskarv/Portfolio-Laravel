aea71e97cba327ac77f7e61ada5b3012f0519346
Vinicius <karvvinicius@gmail.com>
2024-01-30 23:29:47 +0000

    add-about

--- resources/views/about/about.blade.php
+++ resources/views/about/about.blade.php
@@ -1,6 +1,65 @@
 
 <link href="{{ asset('css/about/about.css') }}" rel="stylesheet">
 <div class="container-about">
-   <h1>About</h1>
+   <div class="container">
+      <div class="row row-revers">
+         <div class="col-12 col-sm-6 text-camp">
+            <h3>Programação é a Linguagem</h3>
+            <p>
+               A programação é a linguagem dos computadores, 
+               permitindo que os desenvolvedores criem instruções 
+               precisas para realizar tarefas específicas. 
+            </p>
+         </div>
+         <div class="col-12 col-sm-6">
+            <h5>01</h5>
+         </div>
+         
+      </div>
+      <hr>
+      <div class="row">
+         <div class="col-12 col-sm-6">
+            <h5>02</h5>
+         </div>
+         <div class="col-12 col-sm-6 text-camp">
+            <h3>Programação é a Linguagem</h3>
+            <p>
+               A programação é a linguagem dos computadores, 
+               permitindo que os desenvolvedores criem instruções 
+               precisas para realizar tarefas específicas. 
+            </p>
+         </div>
+      </div>
+      <hr>
+
+      <div class="row row-revers">
+         <div class="col-12 col-sm-6 text-camp">
+            <h3>Programação é a Linguagem</h3>
+            <p>
+               A programação é a linguagem dos computadores, 
+               permitindo que os desenvolvedores criem instruções 
+               precisas para realizar tarefas específicas. 
+            </p>
+         </div>
+         <div class="col-12 col-sm-6">
+            <h5>03</h5>
+         </div>
+      </div>
+      <hr>
+
+      <div class="row">
+         <div class="col-12 col-sm-6">
+            <h5>04</h5>
+         </div>
+         <div class="col-12 col-sm-6 text-camp">
+            <h3>Programação é a Linguagem</h3>
+            <p>
+               A programação é a linguagem dos computadores, 
+               permitindo que os desenvolvedores criem instruções 
+               precisas para realizar tarefas específicas. 
+            </p>
+         </div>
+      </div>
+   </div>
     
 </div>
