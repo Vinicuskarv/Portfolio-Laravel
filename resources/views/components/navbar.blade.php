da08a601c55e594080f980dcf07574de1f3eef8a
Vinicius <karvvinicius@gmail.com>
2024-02-06 22:14:37 +0000

    Commit-navbar-vinicius

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -4,13 +4,16 @@
   </div>
   
   <div class="contant">
-    
+    <div>
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Contact</a>
+      <a href="#">Tools</a>
+      <a href="#">Projects</a>
+    </div>
+    <p>nada aqui</p>
     <div>
       <i class="icon fa-solid fa-earth-americas"></i>
     </div>
-    <label class="switch">
-      <input type="checkbox">
-      <span class="slider"></span>
-    </label>
   </div>
 </nav>
