662807e1ff15190bf5d2db9f7a29b730e47d5bcb
Vinicius <karvvinicius@gmail.com>
2024-02-08 23:17:30 +0000

    edit home

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,16 +2,15 @@
   <div class="text">
     <i class="fa-brands fa-laravel"></i>
   </div>
-  
+
   <div class="contant">
-    <div>
+    <div class="contant-buttons">
       <a href="#">Home</a>
       <a href="#">About</a>
       <a href="#">Contact</a>
       <a href="#">Tools</a>
       <a href="#">Projects</a>
     </div>
-    <p>nada aqui</p>
     <div>
       <i class="icon fa-solid fa-earth-americas"></i>
     </div>
