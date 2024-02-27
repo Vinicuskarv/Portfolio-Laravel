9135452a03bf0dfe1b23d4c83be3211aae59a106
Vinicius <karvvinicius@gmail.com>
2024-02-27 22:41:30 +0000

    add-icons-directions

--- resources/views/components/cardproject.blade.php
+++ resources/views/components/cardproject.blade.php
@@ -1,6 +1,12 @@
 <div class="card-project">
     <b></b>
+    <div class="icons-directions">
+        <ul class="sci">
+            <li><a href="#"><img src="{{ asset('img/icons8-github.gif') }}" alt="icons github"></a></li>
+            <li><a href="#"><img src="{{ asset('img/icons8-site.gif') }}" alt="icons site"></a></li>
+        </ul>
+    </div>
     <div class="content">
-      <p class="title">{{$title}}</span></p>
+      <p class="title">{{$title}}</p>
     </div>
   </div>
--- resources/views/projects/body.blade.php
+++ resources/views/projects/body.blade.php
@@ -1,12 +1,35 @@
 <link href="{{ asset('css/projects/body.css') }}" rel="stylesheet">
-<div class="container">
-    <div class="row">
-        <div class="col">
-            <x-cardproject title="Nada aqui"/>
+<div class="container container-cards">
+    <div class="row justify-content-center">
+        <div class="col-12 col-lg-8">
+            <div class="row">
+                <div class="col">
+                    <x-cardproject title="Tadel"/>
+                </div>
+                <div class="col ">
+                    <x-cardproject title="lucius"/>
+                </div>
+                <div class="col ">
+                    <x-cardproject title="vinicius"/>
+                </div>
+                <div class="col ">
+                    <x-cardproject title="Carvalho"/>
+                </div>
+                <div class="col">
+                    <x-cardproject title="Tadel"/>
+                </div>
+                <div class="col ">
+                    <x-cardproject title="lucius"/>
+                </div>
+                <div class="col ">
+                    <x-cardproject title="vinicius"/>
+                </div>
+                <div class="col ">
+                    <x-cardproject title="Carvalho"/>
+                </div>
+            </div>
         </div>
-        <div class="col">
-            <x-cardproject title="tudo here"/>
-        </div>
+
     </div>
 </div>
 
